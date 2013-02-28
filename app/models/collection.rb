68388c7826cf389aaae9c5222ec22c7cec5f95d0
Lukasz Kornek <lk0001@wp.pl>
2013-02-28 14:53:49 +0100

    [#5] [#6] added validation to List and Collection

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,4 +2,7 @@ class Collection < ActiveRecord::Base
   has_many :lists
 
   attr_accessible :name
+
+  validates :name, presence: true
 end
+
--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,8 @@ class List < ActiveRecord::Base
   has_many :archived_lists
 
   attr_accessible :comment, :items, :name, :version
+
+  validates :name, presence: true
+  validates :version, presence: true
 end
 
