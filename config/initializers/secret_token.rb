32d11b3bcc295c1dcdb2e062ffbdf8ca2627cb24
Lukasz Kornek <lk0001@wp.pl>
2013-02-28 13:49:25 +0100

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listmgr::Application.config.secret_token = 'be46104d661896ecd36ab976d39941c3c13d8312d1286466601329f7dc69f0818a9f47ee6c5e4922952755d55e1e4ad4a2f6cfdba8bca5ad0c9a2c62d604d255'
